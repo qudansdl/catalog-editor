e99185051b7425e31573620fdd378c23eef02c7e
poh <poh@neofect.com>
2020-05-19 07:01:16 +0900

    디랙토리 구조 변경

---
+++ frontend/editor/.eslintrc.js
@@ -0,0 +1,70 @@
+const { resolve } = require('path');
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    es6: true
+  },
+
+
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
+    'plugin:vue-i18n/recommended',
+    'airbnb-typescript/base', // parserOptions.project is requered
+    '@vue/typescript/recommended' // Opinionated ruleset
+  ],
+
+  settings: {
+    'import/resolver':{
+      typescript: {}
+    },
+    'vue-i18n': {
+      localeDir: './src/i18n/*.json'
+    }
+  },
+
+  parserOptions: {
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
+    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
+    // Needed to make the parser take into account 'vue' files
+    extraFileExtensions: ['.vue'],
+    // parser: '@typescript-eslint/parser', // note: it is included in "@vue/typescript[/recommended]"
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
+
+  plugins: [
+    // https://eslint.vuejs.org/user-guide/#vhy-doesn-t-it-work-on-vue-file
+    // required to lint *.vue files
+    'vue',
+    // required to apply rules which need type information
+    '@typescript-eslint'
+  ],
+
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true,
+    'process': true,
+    'Capacitor': true,
+    'chrome': true
+  },
+
+  // add your custom rules here
+  rules: {
+    "max-len": 0,
+    // allow raw text
+    'vue-i18n/no-raw-text': 'off',
+
+    // allow console.log during development only
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-console": "off"
+  }
+}
---
+++ frontend/editor/src/pages/components/background/library.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="layout-padding">
+    <vue-select-image
+      :dataImages="images"
+      :w="'250px'"
+      :h="'200px'"
+      @onselectimage="onSelectImage"/>
+  </div>
+</template>
+
+<script>
+import img1 from '@/assets/img/download.jpg';
+import img2 from '@/assets/img/images.jpg';
+import img3 from '@/assets/img/1.jpg';
+
+import VueSelectImage from '@/components/VueSelectImage/VueSelectImage.vue';
+
+import imageToDataUri from '@/utils/image-to-data-uri';
+
+export default {
+  data() {
+    return {
+      images: [
+        { id: `${img1}1`, src: img1, alt: img1 },
+        { id: `${img2}2`, src: img2, alt: img2 },
+        { id: `${img3}3`, src: img3, alt: img3 },
+
+        { id: `${img1}4`, src: img1, alt: img1 },
+        { id: `${img2}5`, src: img2, alt: img2 },
+        { id: `${img3}6`, src: img3, alt: img3 },
+
+        { id: `${img1}7`, src: img1, alt: img1 },
+        { id: `${img2}8`, src: img2, alt: img2 },
+        { id: `${img3}9`, src: img3, alt: img3 },
+
+        { id: `${img1}10`, src: img1, alt: img1 },
+        { id: `${img2}11`, src: img2, alt: img2 },
+        { id: `${img3}12`, src: img3, alt: img3 },
+
+        { id: `${img1}13`, src: img1, alt: img1 },
+        { id: `${img2}14`, src: img2, alt: img2 },
+        { id: `${img3}15`, src: img3, alt: img3 },
+      ],
+      selected: null,
+    };
+  },
+  components: {
+    VueSelectImage,
+  },
+  methods: {
+    async onSelectImage(img) {
+      this.selected = img;
+      imageToDataUri(img.src, (err, uri) => {
+        this.$emit('imageSelected', uri);
+      });
+    },
+  },
+};
+</script>
